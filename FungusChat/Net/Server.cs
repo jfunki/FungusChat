d243def04ef4cf0261edc410c9f3abed6c3690fc
jonasfunk <jonas.funk45@freenet.de>
2023-04-11 17:27:05 +0200

    Projektdateien hinzufügen.

---
+++ FungusChat/Net/Server.cs
@@ -0,0 +1,97 @@
+﻿using FungusChat.MVVM.Model;
+using FungusChat.MVVM.ViewModel;
+using FungusChat.Net.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+namespace FungusChat.Net
+{
+    internal class Server
+    {
+        TcpClient _client;
+        public PacketReader PacketReader;
+        MainViewModel MainViewModel;
+        public event Action connectedEvent;
+        public event Action userDisconnectEvent;
+        public event Action msgReceivedEvent;
+        private UserModel _userModel;
+        public Server()
+        {
+            _client = new TcpClient();
+            _userModel = new UserModel();
+        }
+
+        // Zum Server connecten
+
+        public void ConnectToServer(string username)
+        {
+            // Schauen ob Client schon verbunden ist
+            if(!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 1860);
+
+                PacketReader = new PacketReader(_client.GetStream());
+
+                if(!string.IsNullOrEmpty(username))
+                {
+                    var connectPacket = new PacketBuilder();
+                    connectPacket.WriteOpCode(0);
+                    connectPacket.WriteMessage(username);
+                    _client.Client.Send(connectPacket.GetPacketBytes());
+                }
+                ReadPackets();
+
+                
+            }
+        }
+
+        private void ReadPackets()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var opcode = PacketReader.ReadByte();
+                    switch (opcode)
+                    {
+                        case 1:
+                            connectedEvent?.Invoke();
+                            break;
+                        case 5:
+                            msgReceivedEvent?.Invoke();
+                            break;
+                        case 10:
+                            userDisconnectEvent?.Invoke();
+                            break;
+                        default:
+                            Console.WriteLine("okay..");
+                            break;
+                    }
+                }
+            });
+        }
+
+        public void SendMessageToServer(string message)
+        {
+            var messagePacket = new PacketBuilder();
+            messagePacket.WriteOpCode(5);
+            messagePacket.WriteMessage(message);
+            _client.Client.Send(messagePacket.GetPacketBytes());
+            MessageToBackupFile(message);
+        }
+
+        public void MessageToBackupFile(string message)
+        {
+            using (StreamWriter writer = File.AppendText(@"C:\Users\ASUS\OneDrive\Desktop\Backup.txt"))
+            {
+                writer.WriteLine($"[{DateTime.Now}]: [{_userModel.Username}]: {message}");
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,74 @@
+﻿using Server.Net.IO;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace Server
+{
+    class Program
+    {
+        static List<Client> _users;
+        static TcpListener _listener;
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 1860);
+            _listener.Start();
+
+
+            while (true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+                BroadCastConnection();
+            }
+            
+            
+            
+        }
+
+        static void BroadCastConnection()
+        {
+            foreach(var user in _users)
+            {
+                foreach(var usr in _users)
+                {
+                    var broadcastpacket = new PacketBuilder();
+                    broadcastpacket.WriteOpCode(1);
+                    broadcastpacket.WriteMessage(usr.Username);
+                    broadcastpacket.WriteMessage(usr.UID.ToString());
+                    user.ClientSocket.Client.Send(broadcastpacket.GetPacketBytes());
+                }
+            }
+        }
+
+        public static void BroadCastMessage(string message)
+        {
+            foreach (var user in _users)
+            {
+                var msgPacket = new PacketBuilder();
+                msgPacket.WriteOpCode(5);
+                msgPacket.WriteMessage(message);
+                user.ClientSocket.Client.Send(msgPacket.GetPacketBytes());
+            }
+        }
+
+        public static void BroadCastDisconnect(string uid)
+        {
+            var disconnectedUser = _users.Where(x => x.UID.ToString() == uid).FirstOrDefault();
+
+            _users.Remove(disconnectedUser);
+            foreach (var user in _users)
+            {
+                var broadcastPacket = new PacketBuilder();
+                broadcastPacket.WriteOpCode(10);
+                broadcastPacket.WriteMessage(uid);
+                user.ClientSocket.Client.Send(broadcastPacket.GetPacketBytes());
+            }
+
+            BroadCastMessage($"[{disconnectedUser.Username}] hat den Chat verlassen");
+        }
+    }
+}
